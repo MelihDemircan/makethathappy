af144b99205f338c97f7f6e8d0e474d3501540e9
MelihDemircan <melih-demircan@hotmail.com>
2019-09-12 22:22:21 +0300

    change rippet

--- src/main/java/com/service/highspeedtrain/service/Gidis.java
+++ src/main/java/com/service/highspeedtrain/service/Gidis.java
@@ -49,7 +49,7 @@ public class Gidis {
 
 //		Tren one = trenList.stream().filter(tren -> tren.getSaat().equals("17:44")).findAny().orElse(null);
 		Tren two = trenList.stream().filter(tren -> tren.getSaat().equals("18:21")).findAny().orElse(null);
-		Tren tre = trenList.stream().filter(tren -> tren.getSaat().equals("19:31")).findAny().orElse(null);
+//		Tren tre = trenList.stream().filter(tren -> tren.getSaat().equals("19:31")).findAny().orElse(null);
 
 		System.out.println(nereden + "/" + nereye + "/" + tarih);
 
@@ -62,10 +62,12 @@ public class Gidis {
 		if (two != null && two.isPulman()) {
 			System.out.println("Sefer Bulundu 18:21");
 			mailService.send("Eskisehir Istanbul  (18:21) (" + tarih + ") " + "(" + two.getPulman() + ")");
-		} else if (tre != null && tre.isPulman()) {
-			System.out.println("Sefer Bulundu 19:31");
-			mailService.send("Eskisehir Istanbul  (19:31) (" + tarih + ") " + "(" + tre.getPulman() + ")");
-		} else {
+		} 
+//		else if (tre != null && tre.isPulman()) {
+//			System.out.println("Sefer Bulundu 19:31");
+//			mailService.send("Eskisehir Istanbul  (19:31) (" + tarih + ") " + "(" + tre.getPulman() + ")");
+//		}
+		else {
 			System.out.println("Sefer Yok (Economi)");
 		}
 
@@ -73,15 +75,15 @@ public class Gidis {
 //			System.out.println("Sefer Bulundu 17:44");
 //			mailService.send("Eskisehir Istanbul  (17:44) (" + tarih + ") " + "(" + one.getBusiness() + ")");
 //		} else
-		if (two != null && two.isBusiness()) {
-			System.out.println("Sefer Bulundu 18:21");
-			mailService.send("Eskisehir Istanbul  (18:21) (" + tarih + ") " + "(" + two.getBusiness() + ")");
-		} else if (tre != null && tre.isBusiness()) {
-			System.out.println("Sefer Bulundu 19:31");
-			mailService.send("Eskisehir Istanbul  (19:31) (" + tarih + ") " + "(" + tre.getBusiness() + ")");
-		} else {
-			System.out.println("Sefer Yok (Business)");
-		}
+//		if (two != null && two.isBusiness()) {
+//			System.out.println("Sefer Bulundu 18:21");
+//			mailService.send("Eskisehir Istanbul  (18:21) (" + tarih + ") " + "(" + two.getBusiness() + ")");
+//		} else if (tre != null && tre.isBusiness()) {
+//			System.out.println("Sefer Bulundu 19:31");
+//			mailService.send("Eskisehir Istanbul  (19:31) (" + tarih + ") " + "(" + tre.getBusiness() + ")");
+//		} else {
+//			System.out.println("Sefer Yok (Business)");
+//		}
 		System.out.println("•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••");
 	}
 }
