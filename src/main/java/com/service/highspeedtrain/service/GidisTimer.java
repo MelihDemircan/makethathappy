b13092f7c8e80072df3e4d9570e2fb64f1bc5977
MelihDemircan <melih-demircan@hotmail.com>
2019-10-16 14:21:38 +0300

    change diff

--- src/main/java/com/service/highspeedtrain/service/GidisTimer.java
+++ src/main/java/com/service/highspeedtrain/service/GidisTimer.java
@@ -16,12 +16,12 @@ public class GidisTimer extends TimerTask {
 		System.out.println(count++);
 
 		try {
-			final String tarih = "13.10.2019";
+			final String tarih = "20.10.2019";
 			final String nereden = "Eskişehir";
 			final String nereye = "İstanbul(Pendik)";
 			List<String> saatler = new ArrayList<>();
-//			saatler.add("16:18");
-//			saatler.add("18:31");
+			saatler.add("16:18");
+			saatler.add("18:31");
 			saatler.add("20:11");
 			saatler.add("20:38");
 			Gidis gidis = new Gidis();
@@ -31,21 +31,21 @@ public class GidisTimer extends TimerTask {
 			e1.printStackTrace();
 		}
 
-//		try {
-//			final String tarih = "11.10.2019";
-//			final String nereden = "İstanbul(Pendik)";
-//			final String nereye = "Eskişehir";
-//
-//			List<String> saatler = new ArrayList<>();
-//			saatler.add("17:44");
-//			saatler.add("18:21");
-//			saatler.add("19:31");
-//			Gidis gidis = new Gidis();
-//			System.out.println("Start Donus Timer");
-//			gidis.start(nereden, nereye, tarih, saatler);
-//		} catch (Exception e1) {
-//			e1.printStackTrace();
-//		}
+		try {
+			final String tarih = "18.10.2019";
+			final String nereden = "İstanbul(Pendik)";
+			final String nereye = "Eskişehir";
+
+			List<String> saatler = new ArrayList<>();
+			saatler.add("17:44");
+			saatler.add("18:21");
+			saatler.add("19:31");
+			Gidis gidis = new Gidis();
+			System.out.println("Start Donus Timer");
+			gidis.start(nereden, nereye, tarih, saatler);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
 
 	}
 }
