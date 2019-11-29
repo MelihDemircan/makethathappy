9bee3c813fabf3fe7d3da6ddbdae8bbd1e1b1261
[Melih Demircan] <melih-demircan>
2019-11-29 08:04:57 +0300

    add date

--- src/main/java/com/service/highspeedtrain/service/Gidis.java
+++ src/main/java/com/service/highspeedtrain/service/Gidis.java
@@ -62,6 +62,15 @@ public class Gidis {
 				mailService.send(nereden + "/" + nereye + " (" + tren.getSaat() + ") (" + tarih + ") " + "("
 						+ tren.getPulman() + ")");
 			}
+			
+			if (tren != null && tren.isBusiness()) {
+				GidisTimer.countSend++;
+				System.out.println("Sefer Bulundu " + nereden + "/" + nereye + "/" + tarih + "/" + tren.getSaat());
+
+				System.out.println("Sefer Bulundu " + tren.getSaat());
+				mailService.send("Business " + nereden + "/" + nereye + " (" + tren.getSaat() + ") (" + tarih + ") " + "("
+						+ tren.getPulman() + ")");
+			}
 		}
 		System.out.println("•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••");
 	}
