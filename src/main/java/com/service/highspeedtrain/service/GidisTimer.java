27cd42d506e6a8636ad478bd02d60b52c7004d79
[Melih Demircan] <melih-demircan>
2019-12-04 10:26:59 +0300

    asd

--- src/main/java/com/service/highspeedtrain/service/GidisTimer.java
+++ src/main/java/com/service/highspeedtrain/service/GidisTimer.java
@@ -16,42 +16,42 @@ public class GidisTimer extends TimerTask {
 		System.out.println(count++ + " / " + countSend);
 
 		//
-//		try {
-//			final String tarih = "29.11.2019";
-//			final String nereden = "İstanbul(Pendik)";
-//			final String nereye = "Eskişehir";
-//			List<String> saatler = new ArrayList<>();
-//			// saatler.add("12:13");
-//			// saatler.add("13:15");
-//			// saatler.add("13:58");
-//			// saatler.add("15:57");
-//			saatler.add("17:44");
-//			saatler.add("18:21");
-//			saatler.add("19:31");
-//			Gidis gidis = new Gidis();
-//			System.out.println("Start Gidis Timer");
-//			gidis.start(nereden, nereye, tarih, saatler);
-//		} catch (Exception e1) {
-//			e1.printStackTrace();
-//		}
-
 		try {
-			final String tarih = "01.12.2019";
-			final String nereden = "Eskişehir";
-			final String nereye = "İstanbul(Pendik)";
-
+			final String tarih = "08.12.2019";
+			final String nereden = "İstanbul(Pendik)";
+			final String nereye = "Eskişehir";
 			List<String> saatler = new ArrayList<>();
+			// saatler.add("12:13");
+			// saatler.add("13:15");
 			// saatler.add("13:58");
-			saatler.add("16:18");
-			saatler.add("18:31");
-			saatler.add("20:11");
-			saatler.add("20:38");
+			// saatler.add("15:57");
+			saatler.add("17:28");
+			saatler.add("18:22");
+			saatler.add("19:32");
 			Gidis gidis = new Gidis();
-			System.out.println("Start Donus Timer");
+			System.out.println("Start Gidis Timer");
 			gidis.start(nereden, nereye, tarih, saatler);
 		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
+
+//		try {
+//			final String tarih = "01.12.2019";
+//			final String nereden = "Eskişehir";
+//			final String nereye = "İstanbul(Pendik)";
+//
+//			List<String> saatler = new ArrayList<>();
+//			// saatler.add("13:58");
+//			saatler.add("16:18");
+//			saatler.add("18:31");
+//			saatler.add("20:11");
+//			saatler.add("20:38");
+//			Gidis gidis = new Gidis();
+//			System.out.println("Start Donus Timer");
+//			gidis.start(nereden, nereye, tarih, saatler);
+//		} catch (Exception e1) {
+//			e1.printStackTrace();
+//		}
 		
 		
 		
