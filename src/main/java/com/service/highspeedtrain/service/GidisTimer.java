0e054704e427f4ffd64f2960d023bb88378d335b
[Melih Demircan] <melih-demircan>
2020-01-21 15:43:58 +0300

    add kon

--- src/main/java/com/service/highspeedtrain/service/GidisTimer.java
+++ src/main/java/com/service/highspeedtrain/service/GidisTimer.java
@@ -16,66 +16,99 @@ public class GidisTimer extends TimerTask {
 		System.out.println(count++ + " / " + countSend);
 
 		//
-//		try {
-//			final String tarih = "17.01.2020";
-//			final String nereden = "İstanbul(Pendik)";
-//			final String nereye = "Eskişehir";
-//			List<String> saatler = new ArrayList<>();
-//			// saatler.add("12:13");
-//			// saatler.add("13:15");
-//			// saatler.add("13:58");
-//			// saatler.add("15:57");
-//			saatler.add("17:28");
-//			saatler.add("18:22");
-//			saatler.add("19:32");
-//			Gidis gidis = new Gidis();
-//			System.out.println("Start Gidis Timer");
-//			gidis.start(nereden, nereye, tarih, saatler);
-//		} catch (Exception e1) {
-//			e1.printStackTrace();
-//		}
+		// try {
+		// final String tarih = "17.01.2020";
+		// final String nereden = "İstanbul(Pendik)";
+		// final String nereye = "Eskişehir";
+		// List<String> saatler = new ArrayList<>();
+		// // saatler.add("12:13");
+		// // saatler.add("13:15");
+		// // saatler.add("13:58");
+		// // saatler.add("15:57");
+		// saatler.add("17:28");
+		// saatler.add("18:22");
+		// saatler.add("19:32");
+		// Gidis gidis = new Gidis();
+		// System.out.println("Start Gidis Timer");
+		// gidis.start(nereden, nereye, tarih, saatler);
+		// } catch (Exception e1) {
+		// e1.printStackTrace();
+		// }
+		//
+		// try {
+		// final String tarih = "19.01.2020";
+		// final String nereden = "Eskişehir";
+		// final String nereye = "İstanbul(Pendik)";
+		//
+		// List<String> saatler = new ArrayList<>();
+		// saatler.add("16:58");
+		// saatler.add("18:36");
+		// saatler.add("19:55");
+		// saatler.add("20:38");
+		// Gidis gidis = new Gidis();
+		// System.out.println("Start Donus Timer");
+		// gidis.start(nereden, nereye, tarih, saatler);
+		// } catch (Exception e1) {
+		// e1.printStackTrace();
+		// }
+
+		////// Konya Konya YHT
 
 		try {
-			final String tarih = "19.01.2020";
-			final String nereden = "Eskişehir";
+			final String tarih = "24.01.2020";
+			final String nereden = "İstanbul(Pendik)";
+			final String nereye = "Konya YHT";
+			List<String> saatler = new ArrayList<>();
+			// saatler.add("12:13");
+			// saatler.add("13:15");
+			// saatler.add("13:58");
+			// saatler.add("15:57");
+//			saatler.add("17:28");
+//			saatler.add("18:22");
+			saatler.add("19:32");
+			Gidis gidis = new Gidis();
+			System.out.println("Start Gidis Timer");
+			gidis.start(nereden, nereye, tarih, saatler);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+
+		try {
+			final String tarih = "26.01.2020";
+			final String nereden = "Konya YHT";
 			final String nereye = "İstanbul(Pendik)";
 
 			List<String> saatler = new ArrayList<>();
-			saatler.add("16:58");
-			saatler.add("18:36");
-			saatler.add("19:55");
-			saatler.add("20:38");
+//			saatler.add("16:58");
+//			saatler.add("18:36");
+//			saatler.add("19:55");
+			saatler.add("18:10");
 			Gidis gidis = new Gidis();
 			System.out.println("Start Donus Timer");
 			gidis.start(nereden, nereye, tarih, saatler);
 		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
-		
-		
-		
-//		try {
-//			final String tarih = "08.12.2019";
-//			final String nereden = "İstanbul(Pendik)";
-//			final String nereye = "Eskişehir";
-//			List<String> saatler = new ArrayList<>();
-//			// saatler.add("12:13");
-//			// saatler.add("13:15");
-//			// saatler.add("13:58");
-//			// saatler.add("15:57");
-//			saatler.add("17:44");
-//			saatler.add("18:21");
-//			saatler.add("19:31");
-//			Gidis gidis = new Gidis();
-//			System.out.println("Start Gidis Timer");
-//			gidis.start(nereden, nereye, tarih, saatler);
-//		} catch (Exception e1) {
-//			e1.printStackTrace();
-//		}
 
+		// try {
+		// final String tarih = "08.12.2019";
+		// final String nereden = "İstanbul(Pendik)";
+		// final String nereye = "Eskişehir";
+		// List<String> saatler = new ArrayList<>();
+		// // saatler.add("12:13");
+		// // saatler.add("13:15");
+		// // saatler.add("13:58");
+		// // saatler.add("15:57");
+		// saatler.add("17:44");
+		// saatler.add("18:21");
+		// saatler.add("19:31");
+		// Gidis gidis = new Gidis();
+		// System.out.println("Start Gidis Timer");
+		// gidis.start(nereden, nereye, tarih, saatler);
+		// } catch (Exception e1) {
+		// e1.printStackTrace();
+		// }
 
-		
-		
 		// try {
 		// final String tarih = "25.10.2019";
 		// final String nereden = "İstanbul(Pendik)";
