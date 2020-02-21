e7841f9619eff458bc4e5c4846aec0ecb0d82730
[Melih Demircan] <melih-demircan>
2020-02-21 09:30:41 +0300

    asd

--- src/main/java/com/service/highspeedtrain/service/GidisTimer.java
+++ src/main/java/com/service/highspeedtrain/service/GidisTimer.java
@@ -23,9 +23,9 @@ public class GidisTimer extends TimerTask {
 			final String nereye = "Eskişehir";
 			List<String> saatler = new ArrayList<>();
 			saatler.add("17:05");
-			saatler.add("17:59");
-			saatler.add("19:09");
-			saatler.add("19:09");
+//			saatler.add("17:59");
+//			saatler.add("19:09");
+//			saatler.add("19:09");
 			Gidis gidis = new Gidis();
 			System.out.println("Start Gidis Timer");
 			gidis.start(nereden, nereye, tarih, saatler);
